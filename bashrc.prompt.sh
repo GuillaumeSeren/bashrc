d32c4a3719acd12d35df06cd8dd34140d3d62869
Guillaume Seren <guillaumeseren@gmail.com>
2015-03-06 01:58:03 +0100

    Fix prompt color / small change to layout.
    - Move the status / number in the begining.
    - Remove the yellow color was not working in rxvt.

--- bashrc.prompt.sh
+++ bashrc.prompt.sh
@@ -9,15 +9,6 @@
 
 function __prompt_command() {
     local EXIT="$?"             # This needs to be first
-    PS1=""
-
-    local RCol='\[\e[0m\]'
-
-    local Red='\[\e[0;31m\]'
-    local Gre='\[\e[0;32m\]'
-    local BYel='\[\e[1;33m\]'
-    local BBlu='\[\e[1;34m\]'
-    local Pur='\[\e[0;35m\]'
 
     if [ $EXIT != 0 ]; then
         bBashStatus="KO"
@@ -31,12 +22,11 @@ function __prompt_command() {
     if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
         debian_chroot=$(cat /etc/debian_chroot)
     fi
-    
+
     # uncomment for a colored prompt, if the terminal has the capability; turned
     # off by default to not distract the user: the focus in a terminal window
     # should be on the output of commands, not on the prompt
     force_color_prompt=yes
-    
     if [ -n "$force_color_prompt" ]; then
         if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
             # We have color support; assume it's compliant with Ecma-48
@@ -47,7 +37,7 @@ function __prompt_command() {
             color_prompt=
         fi
     fi
-    
+
     # Define color for keep the PS1 clean :
     BLACK=$(tput setaf 0)
     RED=$(tput setaf 1)
@@ -68,21 +58,12 @@ function __prompt_command() {
 
     #@TODO: Check git istall in the system
     GIT_PS1_SHOWDIRTYSTATE=true
-    #PS1+="${RCol}@${BBlu}\h ${Pur}\W${BYel}$ ${RCol}"
     if [[ -n $color_prompt && $color_prompt = "yes" ]]; then
         # Color mode
-        #PS1='${debian_chroot:+($debian_chroot)}\[$YELLOW\]\[$BOLD\]\u\[$BLUE\]@\[$GREEN\]\h(\[$BLUE\]\!\[$GREEN\])\[$YELLOW\]:\[$RED\]\[$BOLD\]\w\[$CYAN\] $(__git_ps1 "(%s)")\n\[$YELLOW\]\[$POWDER_BLUE\]\$\[$NORMAL\] ';
-        #PS1='${debian_chroot:+($debian_chroot)}\[$YELLOW\]\u\[$BLUE\]@\[$GREEN\]\h(\[$BLUE\]\!\[$GREEN\])\[$YELLOW\]:\[$RED\]\[$BOLD\]\w\[$CYAN\] $(__git_ps1 "(%s)")\n\[$YELLOW\]\[$POWDER_BLUE\]\$\[$NORMAL\] ';
-       #PS1+="${RCol}@${BBlu}\h ${Pur}\W${BYel}$ ${RCol}"
-       PS1="\[$BOLD\]\[$YELLOW\]\u\[$BLUE\]@\[$GREEN\]\h\[$BLUE\](\[$RED\]$bBashStatus\[$BLUE\]|\[$RED\]\!\[$BLUE\])\[$CYAN\]:\[$GREEN\]\w\[$CYAN\] $(__git_ps1 "(%s)")\n\[$BLUE\]$ \[$NORMAL\]"
+        PS1="\[$BLUE\](\[$RED\]$bBashStatus\[$BLUE\]|\[$RED\]\!\[$BLUE\])\[$GREEN\]\u\[$BLUE\]@\[$GREEN\]\h\[$BLUE\]:\[$GREEN\]\w\[$BLUE\] $(__git_ps1 "(%s)")\n\[$BLUE\]$ \[$NORMAL\]"
     else
         # no color mode
-        GIT_PS1_SHOWDIRTYSTATE=true
-        GitStatus="$(__git_ps1 "(%s)")"
-        # PS1="${debian_chroot:+($debian_chroot)}\u@\h(\!):\w $GitStatus\n\$ "
-        # PS1="----\u@\h($bBashStatus | \!):\w \n\$ "
-        #PS1+=$bBashStatus"${RCol}@${BBlu}\h ${Pur}\W${BYel}$ ${RCol}"
-        PS1="\u@\h($bBashStatus|\!):\w $(__git_ps1 "(%s)")\n $ "
+        PS1="($bBashStatus|\!)\u@\h:\w $(__git_ps1 "(%s)")\n $ "
     fi
 }
 export PROMPT_COMMAND=__prompt_command  # Func to gen PS1 after CMDs
