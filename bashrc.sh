cfd791629a743e708c9da9da44fc990d910f5e00
Guillaume Seren <guillaumeseren@gmail.com>
2015-03-09 16:08:09 +0100

    Fix #3, realpath dependency by using readlink.

--- bashrc.sh
+++ bashrc.sh
@@ -12,7 +12,7 @@
 [ -z "$PS1" ] && return
 
 # First resolve the bashrc link
-BASH_REAL_PATH=$(dirname $(realpath ~/.bashrc))
+BASH_REAL_PATH=$(dirname $(readlink -f ~/.bashrc))
 
 # include set file
 source $BASH_REAL_PATH/bashrc.set.sh
