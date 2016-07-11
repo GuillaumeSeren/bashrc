065245142e8a0b1fcb2034bf3931f82ae302202f
Guillaume Seren <guillaumeseren@gmail.com>
2016-07-11 18:19:52 +0200

    Refactor the user config into libs

---
+++ bashrc.lib-debian.sh
@@ -0,0 +1,15 @@
+# ---------------------------------------------
+# @author Guillaume Seren
+# source  https://github.com/GuillaumeSeren/bashrc
+# file    ~/.bashrc.lib-debian.sh
+# Licence GPLv3
+#
+# Preset for Debian Os
+# Most of the preset are for the admin of the system
+# ---------------------------------------------
+
+# @FIXME Check if checkrestart is available before using it
+alias up='sudo aptitude update && sudo aptitude safe-upgrade && sudo checkrestart'
+alias cl='sudo aptitude remove --purge `deborphan` && sudo aptitude autoclean'
+alias se='sudo aptitude search'
+alias ins='sudo aptitude install'
