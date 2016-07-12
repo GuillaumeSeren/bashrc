86f8cc5d2c6d5491f190923342bc86fe66109f5b
Guillaume Seren <guillaumeseren@gmail.com>
2016-07-12 18:25:17 +0200

    Upgrade up alias command for gentoo

--- bashrc.lib-gentoo.sh
+++ bashrc.lib-gentoo.sh
@@ -8,11 +8,11 @@
 # Most of the preset are for the admin of the system
 # ---------------------------------------------
 
-alias upGrade='sudo emerge-webrsync && sudo emerge -auvDN @world --tree --backtrack=30'
-alias upRebuild='sudo revdep-rebuild -v && sudo revdep-rebuild && sudo emerge --update --newuse --deep @world'
-alias upUpdater='sudo perl-cleaner --all && sudo python-updater'
-alias upClean='sudo eclean-dist -d && sudo emerge --depclean'
 # @TODO Add a check on external dependencies
-alias upCheck='sudo checkrestart'
-alias dup='upGrade && upRebuild && upUpdater && upClean && upCheck'
 alias up='dup'
+alias dup='upGrade && upRebuild && upClean && upCheck'
+alias upGrade='sudo emerge --sync && sudo emerge -avuDN --with-bdeps y --keep-going world'
+alias upRebuild='sudo revdep-rebuild -v && sudo revdep-rebuild && sudo emerge --update --newuse --deep @world && sudo emerge @module-rebuild && sudo emerge @preserved-rebuild'
+alias upClean='sudo eclean-dist -d && sudo emerge -av --depclean'
+alias upCheck='sudo env-update && sudo checkrestart && sudo glsa-check --test all'
+
